bba34d0e9ce0823e390097f01ecca9f0f1715987
Suyog308 <suyog.gaikwad308@gmail.com>
2025-04-14 15:59:11 +0530

    model_eval

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.cnnClassifier.pipeline.stage_3_model_training_pipeline import ModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_4_model_eval import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -31,6 +32,29 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+import mlflow
+import os
+
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/Suyog308/project-dl-main.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="Suyog308"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="2c5055dd91d6b039b7eb56a03f04323441f49527"
+
+mlflow.set_tracking_uri("https://dagshub.com/Suyog308/project-dl-main.mlflow")
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -5,6 +5,7 @@ from src.cnnClassifier.entity.config import PrepareBaseModelConfig
 import tensorflow as tf
 from src.cnnClassifier.entity.config import TrainingConfig
 import os
+from src.cnnClassifier.entity.config import EvaluationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -72,5 +73,15 @@ class ConfigurationManager:
             params_is_augmentation=params.AUGMENTATION,
             params_image_size=params.IMAGE_SIZE
         )
-
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/Suyog308/project-dl-main.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
